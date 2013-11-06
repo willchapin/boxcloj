22e7d639f158eccc8461eac02f852184fbeee974
willchapin <wrchapin@gmail.com>
2013-11-05 23:51:59 -0500

    circles now drift and collide!

--- out/boxcloj/core.js
+++ out/boxcloj/core.js
@@ -26,26 +26,43 @@ boxcloj.core.setAnimFrame.call(null);
 boxcloj.core.canvas = document.getElementById("c");
 boxcloj.core.ctx = boxcloj.core.canvas.getContext("2d");
 boxcloj.core.SCALE = 30;
-boxcloj.core.NUM_CIRCLES = 5;
-boxcloj.core.draw = (function draw(coords){var vec__4688 = coords;var x = cljs.core.nth.call(null,vec__4688,0,null);var y = cljs.core.nth.call(null,vec__4688,1,null);boxcloj.core.ctx.beginPath();
-boxcloj.core.ctx.arc(x,y,100,0,(2 * Math.PI),false);
+boxcloj.core.NUM_CIRCLES = 50;
+boxcloj.core.scale = (function scale(canvas,dim){boxcloj.core.height = (function height(){return (canvas.height / boxcloj.core.SCALE);
+});
+boxcloj.core.width = (function width(){return (canvas.width / boxcloj.core.SCALE);
+});
+if(cljs.core._EQ_.call(null,dim,new cljs.core.Keyword(null,"height","height",4087841945)))
+{return boxcloj.core.height.call(null);
+} else
+{if(cljs.core._EQ_.call(null,dim,new cljs.core.Keyword(null,"width","width",1127031096)))
+{return boxcloj.core.width.call(null);
+} else
+{return null;
+}
+}
+});
+boxcloj.core.centered_rand_int = (function centered_rand_int(n){return (cljs.core.rand_int.call(null,n) - (n / 2));
+});
+boxcloj.core.draw = (function draw(args){var vec__4775 = args;var x = cljs.core.nth.call(null,vec__4775,0,null);var y = cljs.core.nth.call(null,vec__4775,1,null);var r = cljs.core.nth.call(null,vec__4775,2,null);boxcloj.core.ctx.beginPath();
+boxcloj.core.ctx.arc(x,y,r,0,(2 * Math.PI),false);
 boxcloj.core.ctx.fillStyle = "black";
 boxcloj.core.ctx.fill();
 boxcloj.core.ctx.lineWidth = 1;
 boxcloj.core.ctx.strokeStyle = "#335588";
 return boxcloj.core.ctx.stroke();
 });
-boxcloj.core.init = (function init(){var dynamics = Box2D.Dynamics;var collision = Box2D.Collision;var gravity_vec = Box2D.Common.Math.b2Vec2;var shapes = collision.Shapes;var b2body_def = dynamics.b2BodyDef;var b2body = dynamics.b2Body;var b2fixture_def = dynamics.b2FixtureDef;var b2fixture = dynamics.b2Fixture;var b2world = dynamics.b2World;var b2circle = shapes.b2CircleShape;var fix_def = (new b2fixture_def());var body_def = (new b2body_def());var position = body_def.position;boxcloj.core.world = (new b2world((new gravity_vec(0.1,0.3)),true));
+boxcloj.core.init = (function init(){var dynamics = Box2D.Dynamics;var collision = Box2D.Collision;var vec = Box2D.Common.Math.b2Vec2;var shapes = collision.Shapes;var b2body_def = dynamics.b2BodyDef;var b2body = dynamics.b2Body;var b2fixture_def = dynamics.b2FixtureDef;var b2fixture = dynamics.b2Fixture;var b2world = dynamics.b2World;var b2circle = shapes.b2CircleShape;var fix_def = (new b2fixture_def());var body_def = (new b2body_def());var position = body_def.position;boxcloj.core.world = (new b2world((new vec(0,0)),true));
 body_def.type = b2body.b2_dynamicBody;
 var n = 0;while(true){
 if((n < boxcloj.core.NUM_CIRCLES))
-{fix_def.shape = (new b2circle(cljs.core.rand_int.call(null,4)));
+{fix_def.shape = (new b2circle((0.3 + cljs.core.rand.call(null))));
 position.x = cljs.core.rand_int.call(null,30);
 position.y = cljs.core.rand_int.call(null,30);
+body_def.linearVelocity = (new vec(boxcloj.core.centered_rand_int.call(null,4),boxcloj.core.centered_rand_int.call(null,4)));
 boxcloj.core.world.CreateBody(body_def).CreateFixture(fix_def);
 {
-var G__4689 = (n + 1);
-n = G__4689;
+var G__4776 = (n + 1);
+n = G__4776;
 continue;
 }
 } else
@@ -56,15 +73,15 @@ break;
 });
 boxcloj.core.update = (function update(){boxcloj.core.world.Step((1 / 60),10,10);
 boxcloj.core.ctx.clearRect(0,0,1000,1000);
-var node_4690 = boxcloj.core.world.GetBodyList();while(true){
-if(cljs.core.truth_(node_4690))
-{if(cljs.core.truth_(node_4690.GetFixtureList()))
-{boxcloj.core.draw.call(null,boxcloj.core.get_coords.call(null,node_4690));
+var node_4777 = boxcloj.core.world.GetBodyList();while(true){
+if(cljs.core.truth_(node_4777))
+{if(cljs.core.truth_(node_4777.GetFixtureList()))
+{boxcloj.core.draw.call(null,boxcloj.core.get_draw_args.call(null,node_4777));
 } else
 {}
 {
-var G__4691 = node_4690.GetNext();
-node_4690 = G__4691;
+var G__4778 = node_4777.GetNext();
+node_4777 = G__4778;
 continue;
 }
 } else
@@ -74,7 +91,7 @@ break;
 boxcloj.core.world.ClearForces();
 return requestAnimFrame(update);
 });
-boxcloj.core.get_coords = (function get_coords(node){var x = (node.GetPosition().x * (boxcloj.core.canvas.width / boxcloj.core.SCALE));var y = (node.GetPosition().y * (boxcloj.core.canvas.height / boxcloj.core.SCALE));return cljs.core.PersistentVector.fromArray([x,y], true);
+boxcloj.core.get_draw_args = (function get_draw_args(node){var x = (node.GetPosition().x * boxcloj.core.scale.call(null,boxcloj.core.canvas,new cljs.core.Keyword(null,"width","width",1127031096)));var y = (node.GetPosition().y * boxcloj.core.scale.call(null,boxcloj.core.canvas,new cljs.core.Keyword(null,"height","height",4087841945)));var r = (node.GetFixtureList().GetShape().m_radius * boxcloj.core.scale.call(null,boxcloj.core.canvas,new cljs.core.Keyword(null,"width","width",1127031096)));return cljs.core.PersistentVector.fromArray([x,y,r], true);
 });
 boxcloj.core.init.call(null);
 requestAnimFrame(boxcloj.core.update);
--- src/boxcloj/core.cljs
+++ src/boxcloj/core.cljs
@@ -25,12 +25,23 @@
 (def ctx (.getContext canvas "2d"))
 
 (def SCALE 30)
-(def NUM-CIRCLES 5)
+(def NUM-CIRCLES 50)
 
-(defn draw [coords]
-  (let [[x y] coords]
+(defn scale [canvas dim]
+  (defn height []
+    (/ (.-height canvas) SCALE))
+  (defn width []
+    (/ (.-width canvas) SCALE))
+  (cond (= dim :height) (height)
+        (= dim :width) (width)))
+
+(defn centered-rand-int [n]
+  (- (rand-int n) (/ n 2)))
+
+(defn draw [args]
+  (let [[x y r] args]
     (.beginPath ctx)
-    (.arc ctx x y 100 0 (* 2 (.-PI js/Math)) false)
+    (.arc ctx x y r 0 (* 2 (.-PI js/Math)) false)
     (set! (. ctx -fillStyle) "black")
     (.fill ctx)
     (set! (. ctx -lineWidth) 1)
@@ -40,7 +51,7 @@
 (defn init []
   (let [dynamics      (.-Dynamics js/Box2D)
         collision     (.-Collision js/Box2D)
-        gravity-vec   (-> js/Box2D (.-Common) (.-Math) (.-b2Vec2))
+        vec           (-> js/Box2D (.-Common) (.-Math) (.-b2Vec2))
         shapes        (.-Shapes collision)
         b2body-def    (.-b2BodyDef dynamics)
         b2body        (.-b2Body dynamics)
@@ -52,14 +63,16 @@
         body-def      (new b2body-def)
         position      (.-position body-def)]
     
-    (def world (new b2world (new gravity-vec 0.1 0.3) true))
+    (def world (new b2world (new vec 0 0) true))
     (set! (.-type body-def) (.-b2_dynamicBody b2body))
     (loop [n 0]
       (if (< n NUM-CIRCLES)
         (do
-          (set! (.-shape fix-def) (new b2circle (rand-int 4)))
+          (set! (.-shape fix-def) (new b2circle (+ 0.3 (rand))))
           (set! (.-x position) (rand-int 30))
           (set! (.-y position) (rand-int 30))
+          (set! (.-linearVelocity body-def)
+                (new vec (centered-rand-int 4) (centered-rand-int 4)))
           (.CreateFixture (.CreateBody world body-def) fix-def)
           (recur (inc n)))))))
 
@@ -70,15 +83,20 @@
     (if node
       (do
         (if (.GetFixtureList node)
-          (draw (get-coords node)))
+          (draw (get-draw-args node)))
         (recur (.GetNext node)))))
   (.ClearForces world)
   (js/requestAnimFrame update))
 
-(defn get-coords [node]
-  (let [x (* (.-x (.GetPosition node)) (/ (.-width canvas) SCALE))
-        y (* (.-y (.GetPosition node)) (/ (.-height canvas) SCALE))]
-    [x y]))
+(defn get-draw-args [node]
+  (let [x (* (.-x (.GetPosition node)) (scale canvas :width))
+        y (* (.-y (.GetPosition node)) (scale canvas :height))
+        r (-> node
+              (.GetFixtureList)
+              (.GetShape)
+              (.-m_radius)
+              (* (scale canvas :width)))]
+    [x y r]))
 
 (init)
 (js/requestAnimFrame update)
